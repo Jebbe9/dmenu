f40965ea2aaf3f65fe8f6f137a1af3640ba51eb7
Jebbe9 <jebbede@protonmail.com>
2025-03-12 01:47:48 +0200

    tweaking

--- config.def.h
+++ config.def.h
@@ -3,20 +3,22 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int min_width = 400;                    /* minimum width when centered */
 static const float menu_height_ratio = 2.0f;  /* This is the ratio used in the original calculation */
-static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
+static const unsigned int alpha = 0xcc;     /* Amount of opacity. 0xff is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Terminus:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
+/*static const char *colors[SchemeLast][2] = {*/
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	/*[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
-};
+};*/
+
+#include "/home/jere/.cache/wal/colors-wal-dmenu.h"
 
 static const unsigned int alphas[SchemeLast][2] = {
 	[SchemeNorm] = { OPAQUE, alpha },
@@ -33,4 +35,4 @@ static unsigned int lines      = 12;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 2;
+static unsigned int border_width = 3;

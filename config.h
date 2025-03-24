77c4aec3afab548f603408af1ef4294a2f3e80fe
Jebbe9 <jebbede@protonmail.com>
2025-03-24 18:00:54 +0200

    transparency changes

--- config.h
+++ config.h
@@ -5,7 +5,8 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 400;                    /* minimum width when centered */
 static const float menu_height_ratio = 2.0f;  /* This is the ratio used in the original calculation */
-static const unsigned int alpha = 0xcc;     /* Amount of opacity. 0xff is opaque             */
+/*static const unsigned int alpha = 0xcc;*/     /* Amount of opacity. 0xff is opaque             */
+static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Terminus:size=12"
